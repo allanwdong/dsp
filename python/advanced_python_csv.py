fc73c7cbf3ed879cf540fb840f524b90f325b031
allanwdong <allanwdong@users.noreply.github.com>
2017-06-11 20:55:16 -0400

    save 6

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,28 @@
-PLACE YOUR CODE HERE
+
+
+
+
+
+
+Sort Faculty by Last Name
+import csv
+
+def get_dict():
+    dct = {}
+    with open('faculty.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            lastname = row['name'].split(' ')[-1]
+            value = [row[' degree'], 
+                     row[' title'], 
+                     row[' email']]
+
+            if lastname in dct:
+                dct.setdefault(lastname, [])
+                dct[lastname].append(value)
+            else:
+                dct.setdefault(lastname, [])
+                dct[lastname].append(value)
+    return dct
+
+answer = get_dict()
