3c4ac2293cb037ebefe9763342262422051a236a
allanwdong <allanwdong@users.noreply.github.com>
2017-06-15 12:16:52 -0400

    add Count degrees

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,10 +1,45 @@
+*Count Degrees*
+
+import csv
+import re
+from collections import defaultdict
+
+def count_degrees(csv_file_name):
+    degrees = defaultdict(int)
+    
+    phd = re.compile('phd')
+    scd = re.compile('scd')
+    md = re.compile('md')
+    mph = re.compile('mph')
+    bsed = re.compile('bsed')
+    ms = re.compile('ms')
+    jd = re.compile('jd')
+    zero = re.compile('0')
+    ma = re.compile('ma')
+    
+    lst = [phd, scd, md, mph, bsed, ms, jd, zero, ma]
+    dct= {phd : 'PhD', scd : 'ScD', md : 'MD', mph : 'MPH', 
+          bsed : 'BSEd', ms : 'MS', jd : 'JD', zero: '0',
+          ma : 'MA'}
+    with open('faculty.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            x = list(row)[1].lower().replace('.', '')
+            
+            for i in lst:
+                if i.search(x):
+                    degrees[dct[i]] += 1
+    degrees = dict(degrees)
+    return degrees
+                     
+print(count_degrees('faculty.csv'))
 
 
 
 
 
+*Sort Faculty by Last Name*
 
-Sort Faculty by Last Name
 import csv
 
 def get_dict():
